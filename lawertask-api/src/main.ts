441a14d4bc56a0056b55eaef3fef8d4df6c929c7
Diego <diegoooliveira237@gmail.com>
2025-07-30 21:39:35 -0300

    feat:implementa swagger para documentacao da api

---
+++ lawertask-api/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  //Validaçao
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('LaweTaskApi')
+    .setDescription('api para gerenciamento de clientes e tarefas')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();

6ee52f1ab913f436b1006f8bc10af507021f0cbe
Diego <diegoooliveira237@gmail.com>
2025-07-31 23:32:41 -0300

    feat:implementa funcao utilitaria para facilitar reqs a api

---
+++ my-next14-app/app/lib/api.ts
@@ -0,0 +1,41 @@
+const url = "http://localhost:3000/api";
+
+interface ApiResponse<T> {
+  data?: T;
+  error?: string;
+}
+
+export async function apiRequest<T>(
+  endpoint: string,
+  method: string = "GET",
+  body?: any,
+  token?: string
+): Promise<ApiResponse<T>> {
+  try {
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    const response = await fetch(`${url}${endpoint}`, {
+      method,
+      headers,
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return { error: errorData.message || "Erro na requisição" };
+    }
+
+    const data = await response.json();
+    return { data };
+  } catch (error) {
+    return {
+      error: error instanceof Error ? error.message : "Erro desconhecido",
+    };
+  }
+}

e230cde00cc33fb1bfff744d095d736281635a65
Diego <diegoooliveira237@gmail.com>
2025-08-01 21:08:03 -0300

    feat: implementa todas as funçoes realizadas a tasks no front

--- my-next14-app/app/(main)/tasks/page.tsx
+++ my-next14-app/app/(main)/tasks/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import TaskList from "./components/TaskList";
+import { useRouter } from "next/navigation";
 
 export default function Tasks() {
+  const router = useRouter();
+
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <main className="p-3">
-      <div className="grid gap-2">
-        <button className="text-md font-semibold justify-self-end py-2 bg-neutral-600 text-white px-4  rounded-lg shadow hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all">
-          Criar nova task
-        </button>
+      <div className="grid gap-4">
+        <div className="flex justify-end">
+          <button
+            onClick={() => router.push("/tasks/new")}
+            className="text-md font-semibold py-2 bg-neutral-600 text-white px-4 rounded-lg shadow hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
+          >
+            {showForm ? "Cancelar" : "Criar nova task"}
+          </button>
+        </div>
+
         <TaskList />
       </div>
     </main>
